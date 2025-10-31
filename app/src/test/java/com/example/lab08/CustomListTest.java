903eb47ae332abc4518fba764d9d728f2cd85d82
Daniel Minchenko <danielm@Daniels-MacBook-Pro-4.local>
2025-10-31 16:17:12 -0600

    demo1

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -2,6 +2,19 @@ package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+
+    private CustomList makeList() {
+        return new CustomList();
+    }
+
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary)); // this must fail with the stub
+    }
 
 }
